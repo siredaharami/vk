1f38851c9e90c02a25a703947313e00318afab5d
Teamonix <unioxo@outlook.com>
2024-12-05 20:41:59 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+from os import getenv
+from io import BytesIO
+from dotenv import load_dotenv
+# config variables
+if os.path.exists("Config.env"):
+    load_dotenv("Config.env")
+
+API_ID = int(getenv("API_ID", 0))
+API_HASH = getenv("API_HASH", None)
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+STRING_SESSION = getenv("STRING_SESSION", None)
+MONGO_DB_URL = getenv("MONGO_DB_URL", None)
+OWNER_ID = int(getenv("OWNER_ID", 0))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
+START_IMAGE_URL = getenv("START_IMAGE_URL", None)
+
+
