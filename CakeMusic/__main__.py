cf981c859cd7d33f865c0435699f847ce3762769
Teamonix <unioxo@outlook.com>
2024-12-05 21:04:40 +0530

    Update __main__.py

--- CakeMusic/__main__.py
+++ CakeMusic/__main__.py
@@ -1,7 +1,7 @@
 import aiohttp, aiofiles, asyncio, base64, logging
 import os, platform, random, re, socket
 import sys, time, textwrap
-from CakeMusic import app as bot
+from CakeMusic import app as bot, app
 from os import getenv
 from io import BytesIO
 from time import strftime
