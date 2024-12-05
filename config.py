deaf8ccc7857c69ef64df62b7883b8e0190f7d53
Teamonix <unioxo@outlook.com>
2024-12-05 20:53:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ from dotenv import load_dotenv
 if os.path.exists("Config.env"):
     load_dotenv("Config.env")
 
-API_ID = int(getenv("API_ID", 0))
+API_ID = int(getenv("API_ID", "25742938"))
 API_HASH = getenv("API_HASH", None)
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_DB_URL = getenv("MONGO_DB_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", 0))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
+OWNER_ID = int(getenv("OWNER_ID", "7009601543"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002356967761"))
 START_IMAGE_URL = getenv("START_IMAGE_URL", None)
 
 
