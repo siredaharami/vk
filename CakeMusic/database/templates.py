c1ae8eeb08a88ec8eb1de6111ccbd9841449e6af
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-06 23:11:16 +0530

    Create templates.py

---
+++ CakeMusic/database/templates.py
@@ -0,0 +1,10 @@
+import os
+import time
+import logging
+
+from os import getenv
+
+# PM GUARD VARS
+PM_GUARD = bool(getenv("PM_GUARD", True))
+PM_GUARD_TEXT = getenv("PM_GUARD_TEXT", "**🥀 ʜᴇʏ, ɪ ᴀᴍ ᴀɴ ᴀᴅᴠᴀɴᴄᴇᴅ & ꜱᴜᴘᴇʀꜰᴀꜱᴛ ʜɪɢʜ Qᴜᴀʟɪᴛʏ ᴜꜱᴇʀʙᴏᴛ ᴀꜱꜱɪꜱᴛᴀɴᴛ ᴡɪᴛʜ ᴀɴ ᴜᴘɢʀᴀᴅᴇᴅ ᴠᴇʀꜱɪᴏɴ ꜱᴇᴄᴜʀɪᴛʏ ꜱʏꜱᴛᴇᴍ.**")
+PM_GUARD_LIMIT = int(getenv("PM_GUARD_LIMIT", 5))
