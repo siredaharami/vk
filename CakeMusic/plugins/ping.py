ba657c06ff8d272d295ff4c553e59dc92c16346d
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-12-06 11:21:01 +0530

    Create ping.py

---
+++ CakeMusic/plugins/ping.py
@@ -0,0 +1,12 @@
+from CakeMusic import app
+from pyrogram import Client, filters
+import time
+
+# Ping Command
+@app.on_message(filters.command("ping", prefixes=["."]) & filters.me)
+async def ping(client, message):
+    start_time = time.time()
+    reply = await message.reply_text("Pinging...")
+    end_time = time.time()
+    ping_time = round((end_time - start_time) * 1000)  # Convert to ms
+    await reply.edit_text(f"Pong! 🏓\nPing: {ping_time} ms")
