a3c02b9f95a7a79e7c65dd3459211e2baa574946
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-06 22:53:15 +0530

    Update __init__.py

--- CakeMusic/__init__.py
+++ CakeMusic/__init__.py
@@ -14,7 +14,7 @@ bot = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="CakeMusic.plugins")  # Ensure the plugins folder is structured correctly
+    plugins=dict(root="CakeMusic.plugins.Bot")  # Ensure the plugins folder is structured correctly
 )
 
 # Assistant client configuration
@@ -23,7 +23,7 @@ app = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     session_string=STRING_SESSION,  # Ensure STRING_SESSION is valid and properly generated
-    plugins=dict(root="CakeMusic.plugins")  # Plugins shared with bot
+    plugins=dict(root="CakeMusic.plugins.User")  # Plugins shared with bot
 )
 
 
