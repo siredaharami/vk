ca161c86ed513f6b45f5af5aed72c4db92eff31b
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-12-06 14:14:45 +0530

    Update __init__.py

--- CakeMusic/__init__.py
+++ CakeMusic/__init__.py
@@ -2,7 +2,8 @@ from typing import Union, List, Pattern
 from pyrogram import Client, filters as pyrofl
 from config import *
 
-# Bot Client
+
+
 bot = Client(
     name="Bot",
     api_id=API_ID,
@@ -11,21 +12,14 @@ bot = Client(
     plugins=dict(root="CakeMusic.plugins"),
 )
 
-# Userbot Client
-app = Client(
-    name="Userbot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    session_string=STRING_SESSION,  # Replace with your session string
-    plugins=dict(root="CakeMusic.plugins"),  # Optional, same plugins folder
-)
-
-# Command Decorator for Both
 def cdx(commands: Union[str, List[str]]):
     return pyrofl.command(commands, ["/", "!", "."])
+
 
 def cdz(commands: Union[str, List[str]]):
     return pyrofl.command(commands, ["", "/", "!", "."])
 
+
 def rgx(pattern: Union[str, Pattern]):
     return pyrofl.regex(pattern)
+    
