eb95611bfb697824e67051c8c21ea7479484b1a4
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-07 18:16:27 +0530

    Update ping.py

--- CakeMusic/plugins/User/ping.py
+++ CakeMusic/plugins/User/ping.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from config import OWNER_ID
 from CakeMusic import *
+from CakeMusic.plugins.help import HelpMenu
 
 
 @app.on_message(
@@ -16,3 +17,11 @@ async def ping(client, message):
     # Sending the message with the image
     await message.reply_photo(photo=image_path, caption=reply_text)
   
+
+HelpMenu("ping").add(
+    "ping",
+    "<reason>",
+    "Set your status as AFK. When someone mentions' you, the bot will tell them you're currently Offline! You can also use a media by replying to it.",
+    "afk good night!",
+    "To unset afk you can send a message to any chat and it'll automaticslly get disabled! You can use 'afk' in your message to bypass automatic disabling of afk.",
+).info("Away From Keyboard").done()
