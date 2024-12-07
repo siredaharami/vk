aeb8330a974a1df191c9209e531252910a56996a
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-07 11:06:41 +0530

    Update __main__.py

--- CakeMusic/__main__.py
+++ CakeMusic/__main__.py
@@ -22,6 +22,7 @@ from pytgcalls import PyTgCalls, filters as pytgfl
 
 
 from pyrogram import idle, __version__ as pyro_version
+from platform import python_version
 from pytgcalls.__version__ import __version__ as pytgcalls_version
 from pyrogram import __version__ as pyrogram_version
 
