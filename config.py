2f6d01645e325475761fb9518be51c1109d26f2a
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-08 00:04:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,6 @@ MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://hnyx:wywyw2@cluster0.9dxlsl
 OWNER_ID = int(getenv("OWNER_ID", "7009601543"))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002356967761"))
 START_IMAGE_URL = getenv("START_IMAGE_URL", "https://files.catbox.moe/6v7esb.jpg")
-ALIVE_TEXT = getenv("ALIVE_TEXT")
 
 # dont edit this code 
 PLUGINS = {}
