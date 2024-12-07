88d4ca19e432668abf43e15ad9e99737024ef987
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-07 11:13:29 +0530

    Update __init__.py

--- CakeMusic/__init__.py
+++ CakeMusic/__init__.py
@@ -6,7 +6,18 @@ from pytgcalls import PyTgCalls, filters as pytgfl
 from pytgcalls.types import Call, MediaStream, AudioQuality, VideoQuality
 from pyrogram import Client, filters as pyrofl
 from config import *  # Ensure this file contains all required variables: API_ID, API_HASH, BOT_TOKEN, STRING_SESSION
+from pyrogram import idle, __version__ as pyro_version
+from platform import python_version
+from pytgcalls.__version__ import __version__ as pytgcalls_version
+from pyrogram import __version__ as pyrogram_version
 
+
+__version__ = {
+    "Baduserbot": "3.0",
+    "PyTgCalls": pytgcalls_version,
+    "pyrogram": pyrogram_version,
+    "python": python_version(),
+}
 
 # Bot client configuration
 bot = Client(
