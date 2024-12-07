133973ff10fc026826586bb23cb8ce2c8c0a12ab
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-07 17:36:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ import os
 from os import getenv
 from io import BytesIO
 from dotenv import load_dotenv
+from pyrogram import Client, filters
+
 # config variables
 if os.path.exists("Config.env"):
     load_dotenv("Config.env")
