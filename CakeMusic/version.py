6dff65de675f072a65f5637ad973674d03229184
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-07 11:11:14 +0530

    Update version.py

--- CakeMusic/version.py
+++ CakeMusic/version.py
@@ -3,3 +3,10 @@ from platform import python_version
 from pytgcalls.__version__ import __version__ as pytgcalls_version
 from pyrogram import __version__ as pyrogram_version
 
+
+__version__ = {
+    "Baduserbot": "3.0",
+    "PyTgCalls": pytgcalls_version,
+    "pyrogram": pyrogram_version,
+    "python": python_version(),
+}
