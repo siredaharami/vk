cfa1e7fd4b97d22b253207fed876e54fca38e716
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-07 20:36:30 +0530

    Update version.py

--- CakeMusic/version.py
+++ CakeMusic/version.py
@@ -3,17 +3,8 @@ from platform import python_version
 from pytgcalls.__version__ import __version__ as pytgcalls_version
 from pyrogram import __version__ as pyrogram_version
 import aiohttp, aiofiles, asyncio, base64, logging
-from config import *
 
 
 __version__ = {
     "Baduserbot": "2.5",
 }
-
-#pbx
-LOGGER = logging.getLogger("SYSTEM")
-async def main():
-    LOGGER.info("🌐 Checking Required Variables ...")
-    required_env_vars = [
-        "MONGO_DB_URL",
-    ]
