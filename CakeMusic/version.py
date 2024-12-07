48cb7e0a3987eafb63045be10c5602502185963c
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-07 18:02:34 +0530

    Update version.py

--- CakeMusic/version.py
+++ CakeMusic/version.py
@@ -3,6 +3,7 @@ from platform import python_version
 from pytgcalls.__version__ import __version__ as pytgcalls_version
 from pyrogram import __version__ as pyrogram_version
 import aiohttp, aiofiles, asyncio, base64, logging
+from config import *
 
 
 __version__ = {
