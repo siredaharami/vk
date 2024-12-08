b5491f9d57bd9700e260ec2192bd91572f5c24be
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-08 20:28:45 +0530

    Update ping.py

--- CakeMusic/plugins/User/ping.py
+++ CakeMusic/plugins/User/ping.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from config import OWNER_ID
 from CakeMusic import *
-from CakeMusic.plugins.User.help3 import *
+from CakeMusic.plugins.User.help import *
 
 
 @app.on_message(
