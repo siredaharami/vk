cf36c3813cee1403bbd13da60fe64a8e080515dd
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-08 18:50:21 +0530

    Update buttons.py

--- CakeMusic/sukh/buttons.py
+++ CakeMusic/sukh/buttons.py
@@ -1,33 +1,15 @@
-import asyncio
-
-from math import ceil
-from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton
-from typing import Union, List
-
-from CakeMusic import app
-
-
-class EqInlineKeyboardButton(InlineKeyboardButton):
-    def __eq__(self, other):
-        return self.text == other.text
-
-    def __lt__(self, other):
-        return self.text < other.text
-
-    def __gt__(self, other):
-        return self.text > other.text
-
 
 def paginate_plugins(page_n, plugin_dict, prefix, chat=None):
+    from math import ceil
+    
+    # Create the inline keyboard buttons
     if not chat:
         plugins = sorted(
             [
-                EqInlineKeyboardButton(
+                InlineKeyboardButton(
                     x.__NAME__,
-                    callback_data="{}_plugin({})".format(
-                        prefix, x.__NAME__.lower()
-                    ),
+                    callback_data="{}_plugin({})".format(prefix, x.__NAME__.lower())
                 )
                 for x in plugin_dict.values()
             ]
@@ -35,57 +17,28 @@ def paginate_plugins(page_n, plugin_dict, prefix, chat=None):
     else:
         plugins = sorted(
             [
-                EqInlineKeyboardButton(
+                InlineKeyboardButton(
                     x.__NAME__,
-                    callback_data="{}_plugin({},{})".format(
-                        prefix, chat, x.__NAME__.lower()
-                    ),
+                    callback_data="{}_plugin({},{})".format(prefix, chat, x.__NAME__.lower())
                 )
                 for x in plugin_dict.values()
             ]
         )
 
-    pairs = list(zip(plugins[::3], plugins[1::3], plugins[2::3]))
-    i = 0
-    for m in pairs:
-        for _ in m:
-            i += 1
-    if len(plugins) - i == 1:
-        pairs.append((plugins[-1],))
-    elif len(plugins) - i == 2:
-        pairs.append(
-            (
-                plugins[-2],
-                plugins[-1],
-            )
-        )
-
+    # Pagination
     COLUMN_SIZE = 3
-
-    max_num_pages = ceil(len(pairs) / COLUMN_SIZE)
+    max_num_pages = ceil(len(plugins) / COLUMN_SIZE)
     modulo_page = page_n % max_num_pages
-
-    # can only have a certain amount of buttons side by side
-    if len(pairs) > COLUMN_SIZE:
-        pairs = pairs[
-            modulo_page * COLUMN_SIZE : COLUMN_SIZE * (modulo_page + 1)
-        ] + [
-            (
-                EqInlineKeyboardButton(
-                    "❮",
-                    callback_data="{}_prev({})".format(prefix, modulo_page),
-                ),
-                EqInlineKeyboardButton(
-                    " Oᴡɴᴇʀ ",
-                    url=f"tg://openmessage?user_id={app.me.id}",
-                ),
-                EqInlineKeyboardButton(
-                    "❯",
-                    callback_data="{}_next({})".format(prefix, modulo_page),
-                ),
-            )
-        ]
-
-    return pairs
-
-
+    pairs = list(zip(plugins[::COLUMN_SIZE], plugins[1::COLUMN_SIZE], plugins[2::COLUMN_SIZE]))
+    if len(plugins) % COLUMN_SIZE != 0:
+        pairs.append((plugins[-1],))
+    pairs = pairs[modulo_page * COLUMN_SIZE: (modulo_page + 1) * COLUMN_SIZE]
+    
+    # Navigation Buttons
+    return pairs + [
+        (
+            InlineKeyboardButton("❮", callback_data="{}_prev({})".format(prefix, modulo_page)),
+            InlineKeyboardButton(" Oᴡɴᴇʀ ", url="tg://openmessage?user_id={}".format(app.me.id)),
+            InlineKeyboardButton("❯", callback_data="{}_next({})".format(prefix, modulo_page))
+        )
+    ]
